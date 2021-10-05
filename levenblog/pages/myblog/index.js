00de08d1604927fbc056da32bd8f80e63925bb3b
unknown <you@example.comav>
2021-10-05 09:45:23 +0300

    starting

---
+++ levenblog/pages/myblog/index.js
@@ -0,0 +1,27 @@
+const BlogData = () => {
+  
+    return (
+      <div className="home">
+   
+        
+        <div className='person'>
+          <h1>Hey, </h1>
+          <h1> My name is Avi Leventhal</h1>
+          <h3> Welcome to my blog</h3> 
+          <h3> I Participated in different rules in the creation process of several early-stages ventures and Co-founded Currensee,
+               a social network platform for financial traders.</h3> 
+           <h2>In my free-time I enjoy playing tennis and on-line chess.</h2>  
+          </div>
+          <div className="social-links">
+          
+         
+          </div>
+          
+        </div>
+        
+        
+      
+    );
+  }
+   
+  export default BlogData;
